01446f84850fe29a0e7c952a19ab160b04f10e8e
micahjones13 <micahjones13@gmail.com>
2019-08-20 17:15:17 -0600

    components set up

---
+++ portfolio-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+//local
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Micah's Portfolio in Progress </h1>
+    </div>
+  );
+}
+
+export default App;
