f5372382d28d313694cdc70d0396e0a84ce14844
micahjones13 <micahjones13@gmail.com>
2019-09-16 16:51:02 -0600

    added links to the button divs for github and deployment under projects.

--- portfolio-app/src/projects/ProjectCard.js
+++ portfolio-app/src/projects/ProjectCard.js
@@ -2,39 +2,71 @@ import React from "react";
 import styled from "styled-components";
 
 const ProjectCardWrapper = styled.div`
-    // height: 50vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    img{
-        width: 50%;
-    }
+  // height: 50vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  img {
+    width: 50%;
+  }
 `;
 const InfoWrapper = styled.div`
   display: flex;
   flex-direction: column;
   flex-wrap: wrap;
   align-items: center;
-  img{
-      position: relative;
+  .link-div {
+    display: flex;
+    justify-content: space-around;
+    width: 33%;
   }
-  .hoverInfo{
-      position: absolute;
+  .git-div {
+    background: lightgrey;
+    color: black;
+    border: 1px solid black;
+    border-radius: 5px;
+    padding: 2%;
+    cursor: pointer;
+    :hover {
+      transform: scale(1.1);
+    }
   }
-  
+  .dep-div {
+    background: lightgrey;
+    color: black;
+    border: 1px solid black;
+    border-radius: 5px;
+    padding: 2%;
+    cursor: pointer;
+    :hover {
+      transform: scale(1.1);
+    }
+  }
+  a {
+    text-decoration: none;
+    width: 30%;
+  }
+  img {
+    position: relative;
+    z-index: 1;
+  }
 `;
 const ProjectCard = props => {
   return (
     <ProjectCardWrapper>
       <h2>{props.project.title}</h2>
       <InfoWrapper>
-      <img src={props.project.image} alt='project snip'/>
-            <div className = "hoverInfo">
-            <button href={props.project.githubRepo}>Github</button>
-            <button href={props.project.deploymentURL}>Deployed Site</button>
-            </div>
-            <h5>{props.project.description}</h5>
-        </InfoWrapper>
+        <img src={props.project.image} alt="project snip" />
+        <div className="link-div">
+          <a href={props.project.githubRepo}>
+            <div className="git-div">Github</div>
+          </a>
+          <a href={props.project.deploymentURL}>
+            <div className="dep-div">Deployed Site</div>
+          </a>
+        </div>
+        <h5>{props.project.description}</h5>
+      </InfoWrapper>
     </ProjectCardWrapper>
   );
 };
--- portfolio-app/src/projects/ProjectList.js
+++ portfolio-app/src/projects/ProjectList.js
@@ -1,49 +1,40 @@
-import React, { Component } from 'react'
-import ProjectCard from './ProjectCard';
-import { data } from '../assets/data.js';
-import styled from 'styled-components';
-import BlackSand from '../assets/img/blacksand.jpg';
+import React, { Component } from "react";
+import ProjectCard from "./ProjectCard";
+import { data } from "../assets/data.js";
+import styled from "styled-components";
+import BlackSand from "../assets/img/blacksand.jpg";
 
 const ProjectWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    background-image: url(${BlackSand});
+  display: flex;
+  flex-direction: column;
+  background-image: url(${BlackSand});
+  color: white;
+  // height: 150vh;
+  h1 {
+    font-size: 3rem;
+  }
+  hr {
+    width: 85%;
+    border: none;
     color: white;
-    // height: 150vh;
-    h1{
-        font-size: 3rem;
-    }
-    hr{
-        width: 85%;
-        border: none;
-        color: white;
-        background-color: white;
-        height: 5px;
-    }
+    background-color: white;
+    height: 5px;
+  }
 `;
 
 export default class ProjectList extends Component {
-    state = {
-        projects: data
-        
-    }
-    render() {
-        return (
-            <ProjectWrapper>
-            <h1>My Projects</h1>
-            <hr />
-            {
-                this.state.projects.map(project => {
-                    return(
-                        <ProjectCard 
-                            project = {project}
-                            key = {project.id}
-                        />
-                    )
-                })
-            }
-                
-            </ProjectWrapper>
-        )
-    }
+  state = {
+    projects: data
+  };
+  render() {
+    return (
+      <ProjectWrapper>
+        <h1>My Projects</h1>
+        <hr />
+        {this.state.projects.map(project => {
+          return <ProjectCard project={project} key={project.id} />;
+        })}
+      </ProjectWrapper>
+    );
+  }
 }
