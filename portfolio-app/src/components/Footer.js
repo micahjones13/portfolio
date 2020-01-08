06ebeabf9c9c4ebd3cfa2bf98154ac09c03bec68
micahjones13 <micahjones13@gmail.com>
2020-01-08 16:14:44 -0700

    editied footer, added responsive, added maze project

--- portfolio-app/src/components/Footer.js
+++ portfolio-app/src/components/Footer.js
@@ -1,8 +1,10 @@
 import React from "react";
 import styled from "styled-components";
+import BlackSand from "../assets/img/blacksand.jpg";
 
 const ContactMe = styled.div`
-  background: rgb(51, 51, 51) none repeat scroll 0% 0%;
+  // background: rgb(51, 51, 51) none repeat scroll 0% 0%;
+  // background-image: url(${BlackSand});
   color: white;
   a {
     color: white;
@@ -14,30 +16,99 @@ const ContactMe = styled.div`
     }
   }
   display: flex;
-  flex-direction: column;
+  justify-content: center;
   font-family: "Gayathri", sans-serif;
-  h3 {
+  h2 {
     margin-bottom: 0;
   }
-`;
-const FooterWrapper = styled.div`
-  background: rgb(51, 51, 51) none repeat scroll 0% 0%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  // justify-content: space-around;
-
-  ul {
-    list-style-type: none;
-    text-align: left;
-    li {
-      margin-top: 2%;
+  a {
+    margin-left: 5%;
+    margin-right: 5%;
+    @media(max-width: 500px){
+     display: none;
     }
   }
+ 
 `;
-//#00cc66
+const FooterWrapper = styled.div`
+  // background: rgb(51, 51, 51) none repeat scroll 0% 0%;
+  // display: flex;
+  // flex-direction: column;
+  // align-items: center;
+  // justify-content: space-around;
+  width: 70%;
+  // ul {
+  //   list-style-type: none;
+  //   text-align: left;
+  //   li {
+  //     margin-top: 2%;
+  //   }
+  // }
+`;
+
 const Footer = () => {
   return (
+    <ContactMe>
+      <FooterWrapper>
+        <a href="mailto:micahjones13@gmail.com">micahjones13@gmail.com</a>
+
+        <a
+          href="https://www.linkedin.com/in/micah-jones/"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          LinkedIn
+        </a>
+        <a
+          href="https://github.com/micahjones13"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          GitHub
+        </a>
+        <a
+          href="https://resume.creddle.io/resume/haa3tyrytqy"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Resume
+        </a>
+      </FooterWrapper>
+    </ContactMe>
+  );
+};
+
+export default Footer;
+
+/*
+
+        <span>
+          Click <a href="mailto:micahjones13@gmail.com">here </a>
+          to send me an email!
+        </span>
+        <p>Email: micahjones13@gmail.com</p>
+        <p>
+          LinkedIn:{" "}
+          <a
+            href="https://www.linkedin.com/in/micah-jones/"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            https://www.linkedin.com/in/micah-jones/
+          </a>
+        </p>
+        <p>
+          GitHub:{" "}
+          <a
+            href="https://github.com/micahjones13"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            https://github.com/micahjones13
+          </a>
+        </p>
+
+
     <ContactMe>
       <h3>Contact</h3>
       <FooterWrapper>
@@ -79,38 +150,6 @@ const Footer = () => {
         </ul>
       </FooterWrapper>
     </ContactMe>
-  );
-};
-
-export default Footer;
-
-/*
-
-        <span>
-          Click <a href="mailto:micahjones13@gmail.com">here </a>
-          to send me an email!
-        </span>
-        <p>Email: micahjones13@gmail.com</p>
-        <p>
-          LinkedIn:{" "}
-          <a
-            href="https://www.linkedin.com/in/micah-jones/"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            https://www.linkedin.com/in/micah-jones/
-          </a>
-        </p>
-        <p>
-          GitHub:{" "}
-          <a
-            href="https://github.com/micahjones13"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            https://github.com/micahjones13
-          </a>
-        </p>
 
 
 */
