8fc49c8d1dc0cb1e4234bc2f4ddaff9c62dbbb06
Micah Jones <micah.jones@rocketcom.com>
2022-04-18 15:28:59 -0600

    helmet

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 import "./App.css";
 import Banner from "./components/Banner.js";
@@ -11,25 +12,40 @@ import SliderArticle from "./Articles/SliderArticle";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route exact path="/">
-            <Banner />
-            <AboutMe />
-            <ProjectList />
-          </Route>
-          <Route exact path="/articles">
-            <Nav />
-            <Articles />
-          </Route>
-          <Route path="/articles/styling-range-input">
-            <Nav />
-            <SliderArticle />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <HelmetProvider>
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route exact path="/">
+              <Helmet>
+                <title>Micah Jones</title>
+              </Helmet>
+              <Banner />
+              <AboutMe />
+              <ProjectList />
+            </Route>
+            <Route exact path="/articles">
+              <Helmet>
+                <title>Micah Jones - Articles</title>
+              </Helmet>
+              <Nav />
+              <Articles />
+            </Route>
+            <Route path="/articles/styling-range-input">
+              <Helmet>
+                <title>Micah Jones - Slider Styling</title>
+                <link
+                  rel="canonical"
+                  href="https://www.micahjones.dev/styling-range-input"
+                />
+              </Helmet>
+              <Nav />
+              <SliderArticle />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </HelmetProvider>
   );
 }
 
